caec50cf6236009f870413f2539b40370ecbbd2c
Johnny on Flame <johnnyonflame@hotmail.com>
2023-02-03 07:01:14 -0300

    Strip all the deployed shell binaries.

--- util-scripts/package-shell.sh
+++ util-scripts/package-shell.sh
@@ -4,6 +4,7 @@ OUT=$(realpath "out/shell.tar.gz")
 mkdir -p out/tmp_shell/
 cp -r redist/shell/* out/tmp_shell/
 cp out/{dropbearmulti,sftp-server,mdnsd} out/tmp_shell/tools
+${TOOLCHAIN}/bin/arm-linux-gnueabihf-strip out/{dropbearmulti,sftp-server,mdnsd}
 chmod +x out/tmp_shell/special/shell.sh
 (cd out/tmp_shell && tar -czf ${OUT} *)
 rm -rf out/tmp_shell
