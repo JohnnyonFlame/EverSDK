495fb58d7b25b9c6dc038dcc7bb5abe2f60dece1
Johnny on Flame <johnnyonflame@hotmail.com>
2023-02-09 08:20:25 -0300

    Added Freetype 2 libraries.

---
+++ lib-scripts/libfreetype.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+mkdir -p dl/
+mkdir -p pkg/
+wget -nc https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz -O dl/freetype-2.12.1.tar.xz || true
+tar xf dl/freetype-2.12.1.tar.xz -C pkg/
+cd pkg/freetype-2.12.1
+
+export CC="${TOOLCHAIN}/bin/arm-linux-gnueabihf-gcc"
+export CXX="${TOOLCHAIN}/bin/arm-linux-gnueabihf-g++"
+export CFLAGS="-O2"
+export CXXFLAGS="-O2"
+
+meson build/ \
+	--cross-file "${TOOLCHAIN}/meson-cross.ini" \
+	--default-library=static \
+	--prefix="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr" \
+	--buildtype=release
+
+ninja -C build/ install
+
+meson build/ \
+	--reconfigure \
+    --cross-file "${TOOLCHAIN}/meson-cross.ini" \
+	--default-library=shared \
+	--prefix="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr" \
+	--buildtype=release
+
+ninja -C build/ install
