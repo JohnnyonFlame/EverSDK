96e57619a27ffd3879399728dd2f0ece1dae5b24
Johnny on Flame <johnnyonflame@hotmail.com>
2023-06-30 02:55:32 +0000

    Fix libxml2 build.

--- lib-scripts/libxml2.sh
+++ lib-scripts/libxml2.sh
@@ -6,12 +6,14 @@ wget -nc https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.7.tar.xz -O 
 tar xf dl/libxml2-2.9.7.tar.xz -C pkg/
 cd pkg/libxml2-2.9.7
 
+export PKG_CONFIG="${TOOLCHAIN}/bin/arm-linux-gnueabihf-pkg-config"
 export CC="${TOOLCHAIN}/bin/arm-linux-gnueabihf-gcc"
 export CXX="${TOOLCHAIN}/bin/arm-linux-gnueabihf-g++"
 export CFLAGS="-O2"
 export CXXFLAGS="-O2"
 
 ./configure \
+	--with-python=no \
 	--prefix="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr" \
 	--host="arm-linux-gnueabihf"
 
