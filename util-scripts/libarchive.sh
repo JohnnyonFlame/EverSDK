54f75f73aa576ffa18090c473aec44315397c9da
Johnny on Flame <johnnyonflame@hotmail.com>
2023-10-03 01:51:38 +0000

    Workaround issues with bsdtar on fat32.

--- util-scripts/libarchive.sh
+++ util-scripts/libarchive.sh
@@ -6,6 +6,8 @@ mkdir -p dl/
 mkdir -p pkg/
 wget -nc https://www.libarchive.org/downloads/libarchive-3.6.2.tar.xz -O dl/libarchive-3.6.2.tar.xz || true
 tar xf dl/libarchive-3.6.2.tar.xz -C pkg/
+
+cp util-scripts/libarchive-no-paranoid.patch pkg/libarchive-3.6.2/libarchive-no-paranoid.patch
 cd pkg/libarchive-3.6.2
 
 export PKG_CONFIG="${TOOLCHAIN}/bin/arm-linux-gnueabihf-pkg-config"
@@ -14,11 +16,19 @@ export CXX="${TOOLCHAIN}/bin/arm-linux-gnueabihf-g++"
 export CFLAGS="-Os -flto"
 export CFLAGS="${CFLAGS}"
 export CXXFLAGS="${CFLAGS}"
-export LDFLAGS="-Os -s -flto"
+
+patch -p1 < libarchive-no-paranoid.patch
+
+# Not found on stock fw - let's hardlink lzma.
+sed -i 's#-llzma#-l:liblzma.a#g' configure
 
 ./configure \
     --disable-dependency-tracking \
     --host="arm-linux-gnueabihf" \
+    --without-bz2lib \
+    --without-libb2 \
+    --without-selinux \
+    --with-openssl=no \
     --disable-bsdcpio \
     --disable-bsdcat \
     --disable-acl \
