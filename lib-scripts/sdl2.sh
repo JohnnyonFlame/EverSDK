d6e8431960e9118f55b814fde2d5cd18b665a4d4
Johnny on Flame <johnnyonflame@hotmail.com>
2024-05-08 19:13:23 +0000

    Updated SDL2 to SDL2.28.1 with SuPo support.

--- lib-scripts/sdl2.sh
+++ lib-scripts/sdl2.sh
@@ -2,12 +2,11 @@
 
 INSTALL_DIR=$(pwd)/out
 mkdir -p dl/
-mkdir -p pkg/
 mkdir -p out/
-wget -nc https://github.com/libsdl-org/SDL/releases/download/release-2.26.2/SDL2-2.26.2.tar.gz -O dl/SDL2-2.26.2.tar.gz || true
-tar xf dl/SDL2-2.26.2.tar.gz -C pkg/
-cp lib-scripts/SDL2_*.patch pkg/SDL2-2.26.2/
-cd pkg/SDL2-2.26.2
+mkdir -p pkg/SDL2-2.28.1
+wget -nc https://github.com/JohnnyonFlame/SDL-fixkmsdrm/archive/refs/tags/eversdk-r1.tar.gz -O dl/SDL2-2.28.1-eversdk-r1.tar.gz || true
+tar xf dl/SDL2-2.28.1-eversdk-r1.tar.gz --strip-components=1 -C pkg/SDL2-2.28.1
+cd pkg/SDL2-2.28.1
 
 export WAYLAND_SCANNER="${TOOLCHAIN}/bin/wayland-scanner"
 export PKG_CONFIG="${TOOLCHAIN}/bin/arm-linux-gnueabihf-pkg-config"
@@ -17,15 +16,14 @@ export CFLAGS="-Os -Wno-traditional"
 export CXXFLAGS="-Os -Wno-traditional"
 export LDFLAGS="-Os -flto"
 
-patch -p1 < SDL2_evercade_controller.patch
-patch -p1 < SDL2_wl_114.patch
-
 ./configure \
     --prefix="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr" \
     --host="arm-linux-gnueabihf" \
     --enable-static \
     --enable-shared \
     --enable-video-kmsdrm \
+    --disable-pipewire \
+    --disable-oss \
     --disable-video-wayland \
     --disable-video-x11 \
     --disable-video-rpi \
@@ -35,4 +33,4 @@ make -j$(($(nproc)+1)) install
 
 sed 's:^exec_prefix=${prefix}:prefix="${prefix}/arm-linux-gnueabihf/sysroot/usr"\nexec_prefix=${prefix}:' sdl2-config > ${TOOLCHAIN}/bin/sdl2-config
 chmod +x ${TOOLCHAIN}/bin/sdl2-config
-cp "${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr/lib/libSDL2-2.0.so.0.2600.2" "${INSTALL_DIR}/libSDL2-2.0.so.0"
+cp "${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr/lib/libSDL2-2.0.so.0.2800.1" "${INSTALL_DIR}/libSDL2-2.0.so.0"
