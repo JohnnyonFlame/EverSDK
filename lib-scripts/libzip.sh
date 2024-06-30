c38126c23e7468ffedd50427565a633babe8609a
Johnny on Flame <johnnyonflame@hotmail.com>
2024-06-30 22:09:46 +0000

    Add support for static libzip with less dependencies.

--- lib-scripts/libzip.sh
+++ lib-scripts/libzip.sh
@@ -9,6 +9,25 @@ tar xf dl/libzip-1.10.0.tar.xz -C pkg/
 cd pkg/libzip-1.10.0
 
 cmake -Bbuild \
+    -DENABLE_COMMONCRYPTO=OFF \
+    -DENABLE_GNUTLS=OFF \
+    -DENABLE_MBEDTLS=OFF \
+    -DENABLE_OPENSSL=OFF \
+    -DENABLE_WINDOWS_CRYPTO=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN}/armhf.cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_INSTALL_PREFIX="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr"
+
+make -Cbuild -j$(($(nproc)+1)) install
+
+cmake -Bbuild \
+    -DENABLE_COMMONCRYPTO=OFF \
+    -DENABLE_GNUTLS=OFF \
+    -DENABLE_MBEDTLS=OFF \
+    -DENABLE_OPENSSL=OFF \
+    -DENABLE_WINDOWS_CRYPTO=OFF \
+    -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN}/armhf.cmake \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
     -DCMAKE_INSTALL_PREFIX="${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr"
