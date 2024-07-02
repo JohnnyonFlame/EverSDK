6c2bdf4fc6dc23458ad0cf9d642708203457e6d7
Johnny on Flame <johnnyonflame@hotmail.com>
2024-07-02 11:54:58 +0000

    Update SDL2_mixer to v2.8.0.

--- lib-scripts/sdl2_mixer.sh
+++ lib-scripts/sdl2_mixer.sh
@@ -4,9 +4,9 @@ INSTALL_DIR=$(pwd)/out
 mkdir -p dl/
 mkdir -p pkg/
 mkdir -p out/
-wget -nc https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.2/SDL2_mixer-2.6.2.tar.gz -O dl/SDL2_mixer-2.6.2.tar.gz || true
-tar xf dl/SDL2_mixer-2.6.2.tar.gz -C pkg/
-cd pkg/SDL2_mixer-2.6.2
+wget -nc https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.8.0/SDL2_mixer-2.8.0.tar.gz -O dl/SDL2_mixer-2.8.0.tar.gz || true
+tar xf dl/SDL2_mixer-2.8.0.tar.gz -C pkg/
+cd pkg/SDL2_mixer-2.8.0
 
 export PKG_CONFIG="${TOOLCHAIN}/bin/arm-linux-gnueabihf-pkg-config"
 export CC="${TOOLCHAIN}/bin/arm-linux-gnueabihf-gcc"
@@ -23,4 +23,4 @@ export LDFLAGS="-Os -flto"
 
 make clean
 make -j$(($(nproc)+1)) install
-cp "${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr/lib/libSDL2_mixer-2.0.so.0.600.2" "${INSTALL_DIR}/libSDL2_mixer-2.0.so.0"
+cp "${TOOLCHAIN}/arm-linux-gnueabihf/sysroot/usr/lib/libSDL2_mixer-2.0.so.0.800.0" "${INSTALL_DIR}/libSDL2_mixer-2.0.so.0"
